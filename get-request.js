aa44ab444f2f4b369904e25c4542a59840d483e6
Borislav Hadzhiev <opmftw@gmail.com>
2021-09-29 16:15:09 +0300

    add get request handler

---
+++ get-request.js
@@ -0,0 +1,47 @@
+const https = require('https');
+
+function getRequest() {
+  const url = 'https://reqres.in/api/users';
+
+  return new Promise((resolve, reject) => {
+    const req = https.get(url, res => {
+      let rawData = '';
+
+      res.on('data', chunk => {
+        rawData += chunk;
+      });
+
+      res.on('end', () => {
+        try {
+          resolve(JSON.parse(rawData));
+        } catch (err) {
+          reject(new Error(err));
+        }
+      });
+    });
+
+    req.on('error', err => {
+      reject(new Error(err));
+    });
+  });
+}
+
+exports.handler = async event => {
+  try {
+    const result = await getRequest();
+    console.log('result is: 👉️', result);
+
+    // 👇️️ response structure assume you use proxy integration with API gateway
+    return {
+      statusCode: 200,
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    console.log('Error is: 👉️', error);
+    return {
+      statusCode: 400,
+      body: error.message,
+    };
+  }
+};
